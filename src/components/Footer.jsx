740aabc55b1f25e19b506235fc94e023c2341fc9
joaors10 <joaosolano04@gmail.com>
2025-10-25 20:50:52 -0300

    finalize landing page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,28 @@
+import { footerLinks } from "../constants"
+
 const Footer = () => {
     return(
-        <div>footer</div>
+        <footer>
+            <div className="info">
+                <p>Mais formas de comprar: encontre uma Apple Store ou revendedor autorizado perto de você. Ou ligue para 0800-761-0867.</p>
+                <img src="/logo.svg" alt="apple logo" />
+            </div>
+
+            <hr />
+
+            <div className="links">
+                <p>Copyright © 2025 Apple Inc. Todos os direitos reservados.</p>
+
+                <ul>
+                    {footerLinks.map(({label, link}) => (
+                        <li key={label}>
+                            <a href={link}>{label}</a>
+                        </li>
+                    ))}
+                </ul> 
+
+            </div>
+        </footer>
     )
 }
 
